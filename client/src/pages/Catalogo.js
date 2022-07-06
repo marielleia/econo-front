786472d43e5263ad9dea42c98c16dcf55f1bfbed
Maria <maria.carranzavaldes@gmail.com>
2022-07-07 00:06:51 +0200

    first commit

--- client/src/pages/Catalogo.js
+++ client/src/pages/Catalogo.js
@@ -3,26 +3,27 @@ import catalogo from '../experiencias.json'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
 
-export default function Catalogo() {
+function Catalogo() {
   let listaCatalogo = catalogo
 
-  return listaCatalogo.map((el, i) => (
+  return listaCatalogo.map((exp, i) => (
     <div className="catalogo">
-      <Link to={`/detalle/${el.id}`}>
-        <img width={'400px'} heigh={'600px'} className="imagenDestacados" src={el.img} />
+      <Link to={`/detalle/${exp.id}`}>
+        <img width={'400px'} heigh={'600px'} className="imagenDestacados" src={exp.img} />
       </Link>
       <div className="etiquetasDetalle">
-        <Button variant="secondary">{el.eti1}</Button>
-        <Button variant="secondary">{el.eti2}</Button>
-        <Button variant="secondary">{el.eti3}</Button>
+        <Button variant="secondary">{exp.eti1}</Button>
+        <Button variant="secondary">{exp.eti2}</Button>
+        <Button variant="secondary">{exp.eti3}</Button>
       </div>
       <div className='miniDescripcion'>
         <div>
-          <p>{el.eti1}</p>
-          <p>{el.precio}</p>
+          <p>{exp.eti1}</p>
+          <p>{exp.precio}</p>
         </div>
         <Link to="/reserva" className='reserva'>Reserva ahora</Link>
       </div>
     </div>
   ))
 }
+export default Catalogo;
